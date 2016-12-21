2ad36e50173080d7ef2a1e0dd3f9e3a483d3fa6d
Oscarf <k3ther@gmail.com>
2016-12-21 10:50:28 +0100

    Added echossl

---
+++ socketsSeguros/EchoSSL/Client.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
+public class Client {
+	public static void main(String[] args) throws IOException {
+
+		System.out.print("Enter host:");
+		BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+		String host = br1.readLine();
+
+		System.setProperty("javax.net.debug","ssl");
+		System.setProperty("javax.net.ssl.trustStore","AlmacenSRTrust");
+
+		SSLSocketFactory sslsocketfactory = (SSLSocketFactory)
+				SSLSocketFactory.getDefault();
+		SSLSocket socketTCP = (SSLSocket) sslsocketfactory.createSocket(host, 9999);
+
+		InputStream inputStream = System.in;
+		InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+		BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+		OutputStream outputStream = socketTCP.getOutputStream();
+		OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+		BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+
+		InputStream inputStream2 = socketTCP.getInputStream();
+		InputStreamReader inputStreamReader2 = new InputStreamReader(inputStream2);
+		BufferedReader bufferedReader2 = new BufferedReader(inputStreamReader2);
+		String string = null;
+
+		while(!(string = bufferedReader.readLine()).equals("")){
+			bufferedWriter.write(string + "\n");
+			bufferedWriter.flush();
+			System.out.println(bufferedReader2.readLine());
+		}
+
+		socketTCP.close();
+
+	}
+}
---
+++ socketsSeguros/EchoSSL/Server.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+
+public class Server {
+
+	public static void main(String[] args){
+
+		try {			
+			System.setProperty("javax.net.debug","ssl");
+			System.setProperty("javax.net.ssl.keyStore","AlmacenSR");
+			System.setProperty("javax.net.ssl.keyStorePassword","oooooo");
+			SSLServerSocketFactory sslserversocketfactory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+			SSLServerSocket sslserversocket =(SSLServerSocket)
+					sslserversocketfactory.createServerSocket(9999);
+			SSLSocket socket = (SSLSocket) sslserversocket.accept();
+
+			InputStream inputStream = socket.getInputStream();
+			InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+			String string = null;
+
+			OutputStream outputStream = socket.getOutputStream();
+			OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+			BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+
+			while((string = bufferedReader.readLine()) != null){
+				System.out.println(string);
+				Thread.currentThread();
+				Thread.sleep(1000);
+				bufferedWriter.write(string + "\n");
+				bufferedWriter.flush();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
